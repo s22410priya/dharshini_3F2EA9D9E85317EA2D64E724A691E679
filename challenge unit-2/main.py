6319d00918cb66e3303d10e3e253e4b10d571a78
s22410priya <144332798+s22410priya@users.noreply.github.com>
2023-09-20 19:50:01 +0530

    Add files via upload

---
+++ challenge unit-2/main.py
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+class Bank_Account:
+    def name(self):
+        
+        print("Account holder name: c.Priyadharshini")
+        print("Account no: 218164818184894")
+    def _init_(self): #initialize the const
+        self.balance=0
+        print("Welcome to Bank")
+ 
+    def deposit(self):
+        amount=float(input("Enter amount to be Deposited: "))
+        self.balance += amount
+        print("\n Amount Deposited:",amount)
+ 
+    def withdraw(self):
+        amount = float(input("Enter amount to be Withdrawn: "))
+        if self.balance>=amount:
+            self.balance-=amount
+            print("\n You Withdrow:", amount)
+        else:
+            print("\n Insufficient balance  ")
+    def display(self):
+        print("\n Net Available Balance=",self.balance)
+r=Bank_Account()
+r.name()
+r.deposit()
+r.withdraw()
+r.display()
